fbf082f359acc838a96801248d8ebc9cb7e2d88e
raoSahab14127 <eb20103109.raohammadali@gmail.com>
2024-05-17 03:42:06 -0700

    working on app style

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "index" ),
-    path("<int:question_id>/results", view= views.result),
-    path("<int:question_id>", view= views.detail, name= "detail" ),
+    path("<int:question_id>/results", view= views.results, name= "results"),
+    path("<int:question_id>/detail", view= views.detail, name= "detail" ),
     path("<int:question_id>/vote", view= views.vote,  name = "vote"),
 ]
